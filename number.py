336d23e60808a5161ea1f7e726b7452416336df8
Himanshu Soni <himanshusoni1234@gmail.com>
2025-05-24 10:54:02 +0530

    add numbers and strings excersise

---
+++ number.py
@@ -0,0 +1,21 @@
+x = 1
+x = 1.1
+x = 1 + 2j
+print(x)  # (1+2j)
+print(10+3)  # 13
+print(10-3)  # 7
+print(10*3)  # 30
+print(10/3)  # 3.3333333333333335
+print(10//3)  # 3
+print(10 % 3)  # 1
+print(10 ** 3)  # 1000
+print(abs(-10))  # 10
+print(round(2.9))  # 3
+print(round(2.5))  # 3
+print(round(2.5, 1))  # 2.5
+print(int(2.9))  # 2
+print(float(2))  # 2.0
+print(complex(2, 3))  # (2+3j)
+print(1 + 2j + 3 + 4j)  # (4+6j)
+print(1 + 2j * 3 + 4j)  # (1+10j)
+print(1 + 2j * (3 + 4j))  # (-7+6j)
---
+++ strings.py
@@ -0,0 +1,33 @@
+course = 'Python Programming'
+message = """
+Welcome to the course: {}   
+"""
+
+print(message.format(course))  # Welcome to the course: Python Programming
+
+print(course[0])  # P
+print(course[-1])  # g
+print(course[0: 3])  # Pyt
+print(course[0:])  # Python Programming
+print(course[:3])  # Pyt
+print(course[:])  # Python Programming
+print('Python "Programming"')  # Python "Programming"
+print("Python \"Programming\"")  # Python "Programming"
+
+first = 'Himanshu'
+last = 'Soni'
+full = first + ' ' + last
+print(full)  # Himanshu Soni
+full = f"{first} {last}"
+print(full)  # Himanshu Soni
+full = f"{len(first)} {len(last)}"
+print(full)  # 8 4
+
+print(course.upper())  # PYTHON PROGRAMMING
+print(course.lower())  # python programming
+print(course.title())  # Python Programming
+print(course.rstrip())  # Python Programming
+print(course.find('Pro'))  # 10
+print(course.replace('P', 'J'))  # Jython Jrogramming
+print('Python' in course)  # True
+print('Python' not in course)  # True
